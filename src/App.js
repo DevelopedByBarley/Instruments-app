8f3490debb6668268dfe91176857de77b0051131
Árpád <developedbybarley@gmail.com>
2022-10-17 10:04:30 +0200

    Initial commit!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
+import { BrowserRouter, NavLink, Redirect, Route, Switch } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <span className="nav-link">Hangszerek</span>
+            </li>
+            <li className="nav-item">
+              <span className="nav-link">Új hangszer</span>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </BrowserRouter>
   );
 }
 
